5b54f7dd16071c1bdfb6d1766b3b95742132395c
NaNkotsukan <nankotsukan@gmail.com>
2019-07-02 01:57:42 +0900

    進捗だめです

--- test.cu
+++ test.cu
@@ -2,11 +2,45 @@
 #include <chrono>
 #include <random>
 #include <math.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
 
+void inv(float *a, float *b, int n){
 
+    cublasHandle_t handle; 
+    cublasCreate_v2(&handle);
+
+    float *A[1] = {a};
+    float *B[1] = {b};
+    int *info;
+    int *p;
+
+    cudaMalloc(&info, sizeof(int));
+    cudaMalloc(&p, n * sizeof(int));
+    cublasSgetrfBatched(handle, n, A, n, p, info, 1);
+    cublasSgetriBatched(handle, n, A, n, p, B, n, info, 1);
+    cudaFree(info);
+    cudaFree(p);
+    cublasDestroy_v2(handle);
+}
+
+void dot(float *a, float *b, float *y, int n){
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            double r = 0;
+            for(int k = 0; k < n; ++k)
+            {
+                r += a[i*n+k] * b[k*n+j];
+            }
+            y[i*n+j] = abs(r)<0.001?0: r;
+        }
+    }
+}
 
 int main(){
-    int n = 10000;
+    int n = 100;
     // std::cin>>n;
 
     std::chrono::system_clock::time_point  start, end;
@@ -46,37 +80,47 @@ int main(){
 
 
 
-    for(int j = 10;j<n;j*=10){
-        for(int i=1;i<10;++i){
-            int N = j*i;
-            int blockSize = N/32 + (N%32 ? 1 : 0);
-            cudaMemcpy(ref, a, n*n*sizeof(float), cudaMemcpyHostToDevice);
-            start = std::chrono::system_clock::now();
-            
-            cublasSgetriBatched(cublasHandle_t handle,
-                int n,
-                float *Aarray[],
-                int lda,
-                int *PivotArray,
-                float *Carray[],
-                int ldc,
-                int *infoArray,
-                int batchSize);
+    // for(int j = 10;j<n;j*=10){
+    //     for(int i=1;i<10;++i){
+    //         int N = j*i;
+    //         cudaMemcpy(ref, a, n*n*sizeof(float), cudaMemcpyHostToDevice);
+    //         start = std::chrono::system_clock::now();
+    //         inv(a, b, N);
+    //         end = std::chrono::system_clock::now();
+
+
+    //         double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    //         std::cout<<N<<"\t"<<elapsed<<std::endl;
+
+
+    //     }
+
+    // }
+
+    double sum=0;
+    cudaMemcpy(a, a_, n*n*sizeof(float), cudaMemcpyHostToDevice);
+    
+    for(int i = 0;i<n*n;++i)sum+=a_[i];
+    std::cout<<sum<<std::endl;
+    // showArray(a_, n);
+    inv(a, b, n);
+    cudaDeviceSynchronize();
+    cudaMemcpy(buf, a, n*n*sizeof(float), cudaMemcpyDeviceToHost);
+    // showArray(buf, n);
+    cudaMemcpy(b_, b, n*n*sizeof(float), cudaMemcpyDeviceToHost);
+    // showArray(b_, n);
+    std::cout<<"meow"<<std::endl;
+    dot(a_, b_, y_, n);
+    sum = 0;
+    for(int i = 0;i<n*n;++i)sum+=buf[i];
+    std::cout<<sum<<std::endl;
+    sum = 0;
+    for(int i = 0;i<n*n;++i)sum+=y_[i];
+    std::cout<<sum<<std::endl;
+    // showArray(y_, n);
 
 
 
-            cudaDeviceSynchronize();
-
-            end = std::chrono::system_clock::now();
-
-
-            double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
-            std::cout<<N<<"\t"<<elapsed<<std::endl;
-
-
-        }
-
-    }
 
     return 0;
 }
