093d43ac5a6b138873f1693a13d211dc9782bff5
yuta@hacarus.com <yuta@hacarus.com>
2019-07-13 16:57:38 +0900

    copy test

---
+++ copy_speed_test.cu
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <random>
+#include <cusolverDn.h>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+
+__global__ void mkarry(float *a){
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    a[index] = 0;
+}
+
+
+int main(){
+    float *a;
+    int N = 8*32*1000*1000*sizeof(float);
+    cudaMalloc(&a, N);
+    cudaMemset(&a, 0, N);
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
+    cudaMemset(&a, 0, N);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    std::cout<<N<<"\t"<<milliseconds<<std::endl;
+    
+    cudaEventRecord(start);
+
+    mkarry<<<N/32, 32>>>(a);
+    cudaDeviceSynchronize();
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    std::cout<<N<<"\t"<<milliseconds<<std::endl;
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+
+
+}
