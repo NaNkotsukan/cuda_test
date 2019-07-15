45afe8a7afb2ac19735c1dbf54bc44017f2214cb
nankotsukan <nankotsukan@gmail.com>
2019-07-15 17:47:12 +0900

    進捗

--- cublas_test.cu
+++ cublas_test.cu
@@ -16,9 +16,12 @@ void showArray(float *a, int n){
 
 int main(){
     cublasHandle_t cublas_handle;
-    int n_samples, int n_features;
+    cublasCreate(&cublas_handle);
+ 
+    int n_samples, n_features, n_targets;
     n_samples = 10;
     n_features = 15;
+    n_targets = 3;
     float *w_t;
     float *X, *Y;
     cudaMallocManaged(&X, n_samples*n_features*sizeof(float));
@@ -27,26 +30,26 @@ int main(){
     float *coef_matrix;
     cudaMallocManaged(&coef_matrix, n_features * n_features * sizeof(float));
     for(int i = 0;i<n_samples*n_features;++i){
-        X[i] = i;
-        Y[i] = i;
+        X[i] = (float)i;
+        Y[i] = (float)i;
     }
     for(int i = 0;i < n_features * n_features;++i){
         coef_matrix[i]= 0;
     }
     float rho = 1.0;            
 
-
+    float inv_n_samp = 1.0f/n_samples;
     cublasSgemm(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_T,
-        n_features, n_features, n_samples
-        1.0f/n_samples,
+        n_samples, n_samples, n_features,
+        &inv_n_samp,
         X, n_samples,
-        X, n_samples,
-        rho,
+        Y, n_samples,
+        &rho,
         coef_matrix,
         n_features);
 
     cudaDeviceSynchronize();
-    showArray(coef_matrix, n_samples);
+    showArray(coef_matrix, n_features);
     
 
     return 0;
