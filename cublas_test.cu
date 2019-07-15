57455c737d22a725c01e0214a4fe7274b7d68fc1
yuta@hacarus.com <yuta@hacarus.com>
2019-07-15 17:17:50 +0900

    testing blas

---
+++ cublas_test.cu
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <random>
+#include <cusolverDn.h>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+
+
+void showArray(float *a, int n){
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            std::cout<<a[i*n+j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
+int main(){
+    cublasHandle_t cublas_handle;
+    int n_samples, int n_features;
+    n_samples = 10;
+    n_features = 15;
+    float *w_t;
+    float *X, *Y;
+    cudaMallocManaged(&X, n_samples*n_features*sizeof(float));
+    cudaMallocManaged(&Y, n_samples*n_features*sizeof(float));
+    cudaMallocManaged(&w_t, n_features*n_targets*sizeof(float));
+    float *coef_matrix;
+    cudaMallocManaged(&coef_matrix, n_features * n_features * sizeof(float));
+    for(int i = 0;i<n_samples*n_features;++i){
+        X[i] = i;
+        Y[i] = i;
+    }
+    for(int i = 0;i < n_features * n_features;++i){
+        coef_matrix[i]= 0;
+    }
+    float rho = 1.0;            
+
+
+    cublasSgemm(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_T,
+        n_features, n_features, n_samples
+        1.0f/n_samples,
+        X, n_samples,
+        X, n_samples,
+        rho,
+        coef_matrix,
+        n_features);
+
+    cudaDeviceSynchronize();
+    showArray(coef_matrix, n_samples);
+    
+
+    return 0;
+}
+
